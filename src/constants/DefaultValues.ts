c290d591ccff6c48a95ad709cdc93a82f2662a26
adequate <tenoba9@gmail.com>
2024-03-13 22:19:53 +0200

    Add env support

--- src/constants/DefaultValues.ts
+++ src/constants/DefaultValues.ts
@@ -2,9 +2,9 @@ const DefaultValues = {
     Country: "UA",
     Translation: "uk-UA",
     Page: 1,
-    AuthorizationToken: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MmMxYTg2MmY1ZTRmNjQzOTllZDZhOGQ0NTlhYjBmMCIsInN1YiI6IjY1ZTBlODA4NmM0NDljMDE2M2Y5NDdhMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4yXhw3D8n_1VApSdDeIIlOqQ12MZONSgW9I3r7OeD8E",
-    ApiKey: "92c1a862f5e4f64399ed6a8d459ab0f0",
-    ApiKeyLong: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MmMxYTg2MmY1ZTRmNjQzOTllZDZhOGQ0NTlhYjBmMCIsInN1YiI6IjY1ZTBlODA4NmM0NDljMDE2M2Y5NDdhMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4yXhw3D8n_1VApSdDeIIlOqQ12MZONSgW9I3r7OeD8E",
+    AuthorizationToken: import.meta.env.VITE_AUTHORIZATION_TOKEN,
+    ApiKey: import.meta.env.VITE_API_KEY,
+    ApiKeyLong: import.meta.env.VITE_API_KEY_LONG,
     LightTheme: "light",
     DarkTheme: "dark",
     ThemeStorageName: "theme"
